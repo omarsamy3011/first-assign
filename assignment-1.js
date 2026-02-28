5ae815e950b986edd4a91d2e846db2477dd325f1
omar samy <omarsamy3011@gmail.com>
2026-03-01 00:03:42 +0200

    done

--- assignment-1.js
+++ assignment-1.js
@@ -1,20 +1,27 @@
-console.log(+"123" + 7); //1
+//p1
+console.log(+"123" + 7); 
 
+//p2
 let x=0;
 console.log(x==0?"invalid":"valid");
 
+//p3
 for(let i=1;i<=10;i++){
 if(i%2 === 0)continue;
     console.log(i);
 }
 
+//p4
 let y =[1,2,3,4,5]
 let newY = y.filter(ele => ele%2 === 0)
 console.log(newY);
 
+
+//p5
 let o= [1,2,3] ,q=[4,5,6];
 console.log([...o,...q]);
 
+//p6
 let a=2
 switch(a){
     case 1 :
@@ -39,9 +46,10 @@ switch(a){
         console.log("sat");
 }
 
+//p7
 console.log(["a","ab","abc"].map(ele => ele.length));
 
-
+//p8
 let check = no =>{
     if(no%3 === 0 && no%5 === 0)console.log("divisible by 3 and 5");
     else if (no%3 === 0)console.log("divisible by 3 only");
@@ -50,10 +58,11 @@ let check = no =>{
 }
 check(15);
 
+//p9
 let sqr = num=>num*num
 console.log(sqr(5));
 
-
+//p10
 const person ={name:"john" , age:25}
 let dest = function (){
     let {name,age}=person;
@@ -61,22 +70,24 @@ let dest = function (){
 }
 dest(person)
 
+//p11
 let sum = (...y)=> y.reduce((acc,ele)=> acc+ele)
 console.log(sum(1,2,3,4,5));
 
+//p12
 const fn =function(){
     return new Promise((res) => setTimeout(()=>res("success"),3000))
 }
 fn().then(res => console.log(res))
 
+//p13
 let largestNumber = x => x.reduce((acc,ele) => ele>acc ?ele:acc)
-
 console.log(largestNumber([1,2,3,17,20,6]));
 
-
+//p14
 let keys = (m)=>Object.keys(m)
 console.log(keys(person));
 
-
+//p15
 let split = x=>x.split(" ");
 console.log(split("the quick brown fox"));
